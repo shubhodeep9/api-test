839dc90dfa84f26fa16b00b826c51df9cb7ca0f6
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-02-01 23:42:14 +0530

    API init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	_ "api-test/docs"
+	_ "api-test/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
